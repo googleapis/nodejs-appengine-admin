1b86bba89b5d80c3d27d60f232520159e8b80213
Sofia Leon <sofialeon@google.com>
2021-03-22 19:17:42 -0700

    fix synth.py

--- synth.py
+++ synth.py
@@ -30,7 +30,7 @@ for version in versions:
   name, 
   version,
   bazel_target=f"//google/appengine/{version}:appengine-{version}-nodejs")
-  s.copy(library, excludes=["package.json","linkinator.config.json"])
+  s.copy(library, excludes=["package.json","README.md"])
 
 # Copy common templates
 common_templates = gcp.CommonTemplates()
